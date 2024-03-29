628292ffc94af9bfb529b522ef4307f85bd91eca
Calvin <91275935+calvinkochunisg@users.noreply.github.com>
2024-03-29 12:05:50 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,5 @@
 #this will be the homepage, don't change the filename as it is needed by streamlit to acess. Other Classes etc. please add those to separate files and import them.
+
+import streamlit as st
+st.title('Foodplanner in the works!')
+st.subheader('this will be awesome)
